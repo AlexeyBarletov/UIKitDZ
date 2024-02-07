99eabf8a693d1c94f35bcacf167ffb9aee53519b
Alexey Barletov <barletov_92@inbox.ru>
2024-02-07 17:29:47 +0500

    init создал и добавил UIImageView и UIIamge

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -4,8 +4,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var exampleAlertController = UIAlertController()
+    var exampleUIimageView = UIImageView()
+    var exampleUIImage = UIImage(named: "background")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        setupUIimageView()
+    }
+    
+    func setupUIimageView() {
+        exampleUIimageView = UIImageView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height))
+        exampleUIimageView.image = exampleUIImage
+        view.addSubview(exampleUIimageView)
     }
 }
