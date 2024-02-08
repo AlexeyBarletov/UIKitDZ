991eb4fdeac8276343a74486b7409ae10a143cda
Alexey Barletov <barletov_92@inbox.ru>
2024-02-08 19:18:50 +0500

    сделал task_alert_party-2

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -4,8 +4,82 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var exampleButton = UIButton()
+    var copyLabel = UILabel()
+    var copyLabelTwo = UILabel()
+    var copyLabelThree = UILabel()
+    var copyLabelFour = UILabel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        setupButton()
+        setupLabel()
+    }
+
+    // MARK: method button
+
+    func setupButton() {
+        exampleButton = UIButton(frame: CGRect(x: 0, y: 0, width: 355, height: 44))
+        exampleButton.center = view.center
+        exampleButton.layer.cornerRadius = 12
+        exampleButton.backgroundColor = UIColor(red: 76 / 255, green: 216 / 255, blue: 102 / 255, alpha: 1)
+        exampleButton.setTitle("Начать", for: .normal)
+        exampleButton.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        exampleButton.addTarget(self, action: #selector(setupAlertController), for: .touchUpInside)
+        view.addSubview(exampleButton)
+    }
+
+    @objc func setupAlertController() {
+        let alertController = UIAlertController(title: "Введите ваше слово", message: nil, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Отмена", style: .cancel)
+        let actionTwo = UIAlertAction(title: "Ok", style: .default, handler: { _ in
+            guard let textField = alertController.textFields?.first else { return }
+            guard let text = textField.text else { return }
+
+            self.copyLabelThree.text = text
+            self.copyLabelFour.text = String(text.reversed())
+        })
+        alertController.addAction(actionTwo)
+        alertController.addAction(action)
+
+        alertController.addTextField { textField in
+            textField.placeholder = "Введите ваше слово"
+        }
+
+        present(alertController, animated: true)
+    }
+
+    // MARK: method Label
+
+    func setupLabel() {
+        copyLabel = UILabel(frame: CGRect(x: 50, y: 106, width: 275, height: 57))
+        copyLabel.text = "Вы ввели слово"
+        copyLabel.font = .boldSystemFont(ofSize: 16)
+        copyLabel.textColor = UIColor(red: 17 / 255, green: 17 / 255, blue: 17 / 255, alpha: 1)
+        copyLabel.textAlignment = .center
+        view.addSubview(copyLabel)
+
+        copyLabelTwo = UILabel(frame: CGRect(x: 50, y: 225, width: 275, height: 114))
+        copyLabelTwo.text = "А вот что получится, если\nчитать справа налево"
+        copyLabelTwo.numberOfLines = 0
+        copyLabelTwo.font = .boldSystemFont(ofSize: 16)
+        copyLabelTwo.textColor = UIColor(red: 17 / 255, green: 17 / 255, blue: 17 / 255, alpha: 1)
+        copyLabelTwo.textAlignment = .center
+        view.addSubview(copyLabelTwo)
+
+        copyLabelThree = UILabel(frame: CGRect(x: 50, y: 157, width: 275, height: 57))
+        copyLabelThree.text = ""
+        copyLabelThree.font = .boldSystemFont(ofSize: 16)
+        copyLabelThree.textColor = UIColor(red: 17 / 255, green: 17 / 255, blue: 17 / 255, alpha: 1)
+        copyLabelThree.textAlignment = .center
+        view.addSubview(copyLabelThree)
+
+        copyLabelFour = UILabel(frame: CGRect(x: 50, y: 295, width: 275, height: 57))
+        copyLabelFour.text = ""
+        copyLabelFour.font = .boldSystemFont(ofSize: 16)
+        copyLabelFour.textColor = UIColor(red: 17 / 255, green: 17 / 255, blue: 17 / 255, alpha: 1)
+        copyLabelFour.textAlignment = .center
+        view.addSubview(copyLabelFour)
     }
 }
