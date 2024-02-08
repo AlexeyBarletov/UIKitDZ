d39f909bdefab5ac52800056047da65eb9fbe5d0
Alexey Barletov <barletov_92@inbox.ru>
2024-02-09 01:42:43 +0500

    Revert "сделал task_switch"
    This reverts commit 680d14f4072d7942678d5e923c848f8cda21caf4.

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -4,17 +4,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var showButtonOutlet: UIButton!
-    @IBOutlet var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("")
-    }
-
-    //MARK: method button
-    
-    @IBAction func showTextButton(_ sender: Any) {
-        textField.isSecureTextEntry = !showButtonOutlet.isSelected
-        showButtonOutlet.isSelected = !showButtonOutlet.isSelected
+        view.backgroundColor = .white
     }
 }
