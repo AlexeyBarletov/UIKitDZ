eee2e799bae61b2aa4e44c6e1b34214cfaca8706
Alexey Barletov <barletov_92@inbox.ru>
2024-02-10 16:19:24 +0500

    создал private func and MARK

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -6,10 +6,24 @@ import UIKit
 class ViewController: UIViewController {
     
     var copyImageView = UIImageView()
-    var copyImage = UIImageView()
+    var copyImage = UIImage(named: "image")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        addViewDidLoad()
     }
+    
+    private func addViewDidLoad() {
+        setupImageView()
+        
+    }
+    
+    //MARK: Method Private
+    
+    private func setupImageView() {
+        copyImageView = UIImageView(frame: CGRect(x: 70, y: 125, width: 125, height: 215))
+        view.addSubview(copyImageView)
+    }
+    
 }
