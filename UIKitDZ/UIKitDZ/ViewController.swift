cd4b748b01d9c06d1fb51a54ce36637b8c7265c0
Alexey Barletov <barletov_92@inbox.ru>
2024-02-10 15:43:40 +0500

    создал два экземпляра UIImageView and UIImage

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -4,6 +4,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var copyImageView = UIImageView()
+    var copyImage = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
